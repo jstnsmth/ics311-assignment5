e9661d73752c32c3b8cb555b7b4335d14c0c7566
Zelda <zeldac@hawaii.edu>
2024-11-01 21:45:32 -1000

    updates

--- src/Main.java
+++ src/Main.java
@@ -71,3 +71,11 @@ public class Main {
 
 
 
+public class Main {
+    public static void main(String[] args) {
+        // Just for testing purposes
+        Graph graph = new Graph();
+        graph.loadGraphData("data/island-data.txt");
+        graph.displayGraph();
+    }
+}
